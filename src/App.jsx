d1e035face05db51d44580a14f3c09564f6edfb7
mobin zaki <1380765@cityplym.ac.uk>
2023-04-05 20:30:07 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link, Outlet} from "react-router-dom";
+import Homepage from './components/homepage/homepage'
+import Card from './components/Quadratic/Quadratic'
+import Probability from './components/Binomial/Binomial';
+import Poisson from './components/poisson/poisson';
+import Normal from './components/NormalDistribution/Normal';
+import ChiSquared from './components/ChiSquared/ChiSquared';
+
+function App() {
+
+  return (
+    <>
+
+   <Router>
+          
+          <Outlet/>
+          <Routes>
+                <Route path= "/" element={<Homepage/>} exact/>
+                <Route path="Quadratic" element={<Card />} exact/>
+                <Route path="Pos" element={<Probability/>} exact/>
+                <Route path="Poisson" element={<Poisson/>} exact/>
+                <Route path="Normal" element={<Normal/>} exact/>
+                <Route path="ChiSquared" element={<ChiSquared/>} exact/>
+           </Routes>
+        </Router>
+  
+    </>
+    
+  )
+}
+
+export default App
